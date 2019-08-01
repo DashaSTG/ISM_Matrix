86c51534432bde7602c247e7948a38502384247c
DashaSTG <stgstsimple@gmail. com>
2019-08-01 13:37:08 +0300

    ready

---
+++ ISM_Matrix/Library.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLibrary
+{
+    public class Library
+    {
+        public int Task1(int[,] matrix)
+        {
+            int maxModNumber = int.MinValue;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (Math.Abs(matrix[i, j]) > maxModNumber)
+                    {
+                        maxModNumber = Math.Abs(matrix[i, j]);
+                    }
+                }
+
+            return maxModNumber;
+        }
+
+        public int[,] Task2(int[,] matrix)
+        {
+            int[,] matrix2 = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix2[j, i] = matrix[i, j];
+                }
+
+            return matrix2;
+        }
+
+        public int Task3(int[,] matrix)
+        {
+            int sumTask3 = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        sumTask3++;
+                    }
+                }
+
+            return sumTask3;
+        }
+
+        public int Task4(int[,] matrix)
+        {
+            int linesWithoutZero = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        linesWithoutZero--;
+                        break;
+                        
+                    }
+
+                }
+                linesWithoutZero++;
+            }
+
+            return linesWithoutZero;
+        }
+
+        public int Task5(int[,] matrix)
+        {
+            int columnsWithZero = 0;
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        columnsWithZero++;
+                        break;
+                    }
+                }
+
+            }
+
+            return columnsWithZero;
+        }
+
+        public int Task6(int[,] matrix)
+        {
+            int countSameElements = 0;
+            int lineIndex = -1;
+            int maxSeria = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i, j] == matrix[i, j + 1])
+                    {
+                        countSameElements++;
+                    }
+
+                    if (countSameElements > maxSeria)
+                    {
+                        maxSeria = countSameElements;
+                        lineIndex = i;
+                    }
+                }
+            }
+
+            return lineIndex;
+        }
+
+
+        public int Task7(int[,] matrix)
+        {
+            int mainDiagonalSum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        mainDiagonalSum += matrix[i, j];
+                    }
+                }
+            return mainDiagonalSum;
+        }
+
+        public int Task8(int[,] matrix)
+        {
+            int additionalDiagonalSum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i + j == matrix.GetLength(0) - 1)
+                    {
+                        additionalDiagonalSum += matrix[i, j];
+                    }
+                }
+            return additionalDiagonalSum;
+        }
+
+        public int Task9(int[,] matrix)
+        {
+            int sumTask9 = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        sumTask9 += matrix[i, j];
+                    }
+                }
+            return sumTask9;
+        }
+    }
+}
---
+++ task_console/console.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatrixLibrary;
+
+namespace task_console
+{
+    class console
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть кiлькiсть рядкiв: ");
+            int lines = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть кiлькiсть стовпчикiв: ");
+            int columns = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[lines, columns];
+
+            Random random = new Random();
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = random.Next(-100, 100);
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Library solver = new Library();
+
+            var maxModNumber = solver.Task1(matrix);
+            Console.WriteLine($"Максимальне за модулем число = {maxModNumber}");
+
+            var matrix2 = solver.Task2(matrix);
+            Console.WriteLine($"Транспонована матриця: ");
+            for (int i = 0; i < matrix2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix2[i, j]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            var sumTask3 = solver.Task3(matrix);
+            Console.WriteLine($"Сума додатнiх елементiв масиву = {sumTask3}");
+
+
+            var linesWithoutZero = solver.Task4(matrix);
+            Console.WriteLine($"Кiлькiсть рядкiв без нуля = {linesWithoutZero}");
+
+
+            var columnsWithZero = solver.Task5(matrix);
+            Console.WriteLine($"Кiлькiсть стовпчикiв з нулем = {columnsWithZero}");
+
+
+            var lineIndex = solver.Task6(matrix);
+            if (lineIndex == -1)
+            {
+                Console.WriteLine("Не iснує");
+            }
+            Console.WriteLine($"Номер рядка з найдовшою серiєю однакових елементiв {lineIndex}");
+
+
+            if (lines == columns)
+            {
+                var mainDiagonalSum = solver.Task7(matrix);
+                Console.WriteLine($"Сума елементiв головної дiагоналi матрицi = {mainDiagonalSum}");
+            }
+
+
+            if (lines == columns)
+            {
+                var additionalDiagonalSum = solver.Task8(matrix);
+                Console.WriteLine($"Сума елементiв побiчної дiагоналi матрицi = {additionalDiagonalSum}");
+            }
+
+
+            var sumTask9 = solver.Task9(matrix);
+            Console.WriteLine($"Сума елементiв, якi розташованi на позицiях з парною сумою iндексiв = {sumTask9}");
+        }
+    }
+}
